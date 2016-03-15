30bfec73963a4952c31109a50d7ef76298a42043
Alex Bernard <itsalexbernard@gmail.com>
2016-03-15 22:28:23 +0100

    Basic file structure setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>MAIV</title>
+</head>
+<body>
+
+  <script src="js/script.js" type="text/javascript" charset="utf-8" async defer></script>
+</body>
+</html>
