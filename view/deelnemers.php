ce973c947a753b3d6511aac368e0b29e713cdb63
suvd <suzan.van.dijck@student.howest.be>
2016-03-17 22:39:16 +0100

    deelnemers html

--- view/deelnemers.php
+++ view/deelnemers.php
@@ -7,7 +7,61 @@
   <link rel="stylesheet" type="text/css" href="<?php echo $basepath;?>/css/style.css"/>
 </head>
 <body>
-  <h1>DEELNEMERS</h1>
+  <nav>
+    <ul>
+      <li><a href="http://www.boek.be/">Boek.be</a></li>
+      <li><a href="boek.php">Het Boek</a></li>
+      <li>Concurenten</li>
+      <li><a href="deelnemen.php">Deelnemen</a></li>
+    </ul>
+  </nav>
+  <header>
+    <h1>Be ware</h1>
+    <h1>The concurents</h1>
+    <h1>are there</h1>
+  </header>
+  <main>
+    <section>
+      <ul>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+        <li><img src="" alt=""></li>
+      </ul>
+    </section>
+    <section>
+      <nav>
+        <ul>
+          <li><</li>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>></li>
+        </ul>
+      </nav>
+      <div>
+      </div>
+    </section>
+  </main>
+  <footer>
+    <nav>
+      <ul>
+        <li><a href="">Het boek</a></li>
+        <li><a href="">Concurenten</a></li>
+        <li><a href="">Deelnemen</a></li>
+      </ul>
+      <ul>
+        <li><a href="http://www.boek.be/">Boek.be</a></li>
+        <li><a href="https://www.facebook.com/Boekbe-301703439127/?fref=ts">facebook</a></li>
+        <li><a href="https://twitter.com/boekbe">twitter</a></li>
+      </ul>
+    </nav>
+  </footer>
   <script>
     window.app = window.app || {};
     window.app.basePath = '<?php echo $basepath; ?>'
